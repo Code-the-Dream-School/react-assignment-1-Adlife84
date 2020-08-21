d975636c6521d7dc429c78cbd28d11eea51f58fd
Andrei <andrei@olgas-imac.home>
2020-08-21 11:37:25 -0400

    Added check that cell is free?

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Info from './Info';
 import X from '../images/1.png';
 import O from '../images/0.png';
+import N from '../images/None.png';
 
 
 class Game extends Component {
@@ -17,47 +18,55 @@ class Game extends Component {
 
     board: [
       [
-        { id: [0, 0], player: X },
-        { id: [0, 1], player: X },
-        { id: [0, 2], player: X }
+        { id: [0, 0], player: N },
+        { id: [0, 1], player: N },
+        { id: [0, 2], player: N }
       ],
       [
-        { id: [1, 0], player: X },
-        { id: [1, 1], player: O },
-        { id: [1, 2], player: O }
+        { id: [1, 0], player: N },
+        { id: [1, 1], player: N },
+        { id: [1, 2], player: N }
       ],
       [
-        { id: [2, 0], player: O },
-        { id: [2, 1], player: O },
-        { id: [2, 2], player: O }
+        { id: [2, 0], player: N },
+        { id: [2, 1], player: N },
+        { id: [2, 2], player: N }
       ]
     ]
   };
 
   //Change a player turn
   changeCurrentPlayer = () => {
-    this.state.currentPlayer === 'player1'? 
-    this.setState(prevState => {prevState.currentPlayer = 'player2'}): 
-    this.setState(prevState => {prevState.currentPlayer = 'player1'});
+    this.state.currentPlayer === 'player1' ?
+      this.setState(prevState => { prevState.currentPlayer = 'player2' }) :
+      this.setState(prevState => { prevState.currentPlayer = 'player1' });
     console.log("I have change players", this.state.currentPlayer);  //Test that state changed
   }
+
+
 
   //Assign image Player to cell
   assignPlayerToCell = (id) => {
 
-    if(this.state.currentPlayer === "player1"){
-      this.setState( prevState => { 
-        prevState.board[id[0]][id[1]].player = O; 
-        return prevState;
-      });
+    if (this.state.board[id[0]][id[1]].player === N) {
+      if (this.state.currentPlayer === "player1") {
+        this.setState(prevState => {
+          prevState.board[id[0]][id[1]].player = O;
+          return prevState;
+        });
+      } else {
+        this.setState(prevState => {
+          prevState.board[id[0]][id[1]].player = X;
+          return prevState;
+        });
+      }
+
     } else {
-      this.setState( prevState => { 
-        prevState.board[id[0]][id[1]].player = X; 
-        return prevState;
-      });
+      alert("Cell is not free, click on other cell");
     }
-  
-    this.changeCurrentPlayer();
+
+
+    this.changeCurrentPlayer(); //Change player each next turn
   }
 
 
@@ -66,11 +75,11 @@ class Game extends Component {
   render() {
 
     // this.assignPlayerToCell(3); // Test my method onClick to Cell
-  
+
 
     return (
       <div>
-        
+
         {/* <Header /> */}
         {/* <Info /> */}
         <Board
