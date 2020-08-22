936d31adced3dce69626e416dc3e7e9176ddb168
Andrei <andrei@olgas-imac.home>
2020-08-21 22:24:31 -0400

    Add isWin methid

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ class Game extends Component {
       name: 'Olga ',
       steps: []
     },
-    
+
     currentPlayer: "player1",
     isWin: "None",
 
@@ -45,10 +45,11 @@ class Game extends Component {
 
 
     winCombinations: [
-      [0, 1, 2],
-      [0, 0, 0],
-      [1, 1, 1], 
-      [2, 2, 2]   
+      '0,1,2',
+      '0,0,0',
+      '1,1,1',
+      '2,2,2',
+      '2,1,0'
     ]
   };
 
@@ -60,9 +61,39 @@ class Game extends Component {
     console.log("I have change players", this.state.currentPlayer);  //Test that state changed
   }
 
-  //Check is it WIN
-  isWin = () => {
-    console.log('Hello from isWin');
+  //Check is it WIN (compare two arrays beatwean each other if it's the same you win)
+  isWin = (player) => {
+    console.log('Hello from isWin', this.state.player1.steps);
+    let playerArray = player.steps;
+    let playerArrayString = playerArray.toString()
+    let winArray = this.state.winCombinations;
+
+    console.log(playerArray);
+    console.log(playerArrayString);
+
+    console.log(winArray.indexOf(playerArrayString));
+
+    if (winArray.indexOf(playerArrayString) === -1) {
+      console.log("Game over!");
+    } else {
+      console.log("You Win!", winArray.indexOf(playerArrayString));
+      this.state.isWin = player.name; //Put name of winer in the state
+      alert(`Congratulation you ${this.state.isWin}! You Won!!!`);
+    }
+
+
+    // Here i try to compare two arrays
+    // let winCombination = [0, 0, 0];
+
+    // // If I have all three the same numbers it is means that our arrays are the same
+    // for (var i = 0; i < winCombination.length; i++) {
+    //   if (playerArray.indexOf(winCombination[i]) === -1) {
+    //     console.log("It is not the same: ", winCombination[i], playerArray); //Do this if not the same
+    //     return console.log('Game over');
+    //   } else {
+    //     console.log("Win combination is: ", winCombination, playerArray); //Do this if index the same
+    //   };
+    // };
   }
 
 
@@ -77,6 +108,8 @@ class Game extends Component {
           prevState.board[id[0]][id[1]].player = O;
           prevState.player1.steps.push(id[1]);
           console.log('Player 1', this.state.player1.steps);
+          this.isWin(this.state.player1); //Check maybe Player1 is Win
+         
           return prevState;
         });
       } else {
@@ -84,17 +117,19 @@ class Game extends Component {
           prevState.board[id[0]][id[1]].player = X;
           prevState.player2.steps.push(id[1]);
           console.log('Player 2', this.state.player2.steps);
+          this.isWin(this.state.player2); //Check maybe Player2 is Win
+     
           return prevState;
         });
       }
-
     } else {
       alert("Cell is not free, click on other cell");
     }
 
-    
-    this.isWin(); //Check maybe currentPlayer is Win
     this.changeCurrentPlayer(); //Change player each next turn
+    console.log("I work first");
+
+
   }
 
 
